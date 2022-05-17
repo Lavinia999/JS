b14bd1feb5b54fae80fcdee47a0130e3ed44091b
Mihail Terenti <maikttt@gmail.com>
2022-05-17 12:15:16 +0300

    add a few improvements

--- plugin.js
+++ plugin.js
@@ -1,46 +1,37 @@
 (function($){
-$.fn.slider = function(options){
-    options = $.extended({
-        slider : function(option){
-            return $('<li>',{
-                html : li.image()
-            });
-        },
-        className : ''
-    },options);
+$.fn.LaviniaSlider = function(options) {
+    const $slider = $('#slider');
+    const $slideContainer = $('.slides', $slider);
+    const $slides = $('.slide', $slider);
+    let currentSlide = 1;
+    let interval;
 
-    return this.each(function(){
-        var width = 670;
-        var animationSpeed = 1000;
-        var pause = 2090;
-        var currentSlide = $(this);
-        var $slider = $('#slider');
-        var $slideContainer = $('.slides', $slider);
-        var $slides = $('.slide', $slider);
-    });
-    var interval;
-    
+    options = $.extend({
+        width: 670,
+        animationSpeed: 1000,
+        pause: 2090,
+    }, options);
+
     function startSlider() {
         interval = setInterval(function() {
-            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+            $slideContainer.animate({'margin-left': '-=' + options.width}, options.animationSpeed, function() {
                 if (++currentSlide === $slides.length) {
                     currentSlide = 1;
                     $slideContainer.css('margin-left', 0);
                 }
             });
-        }, pause);
+        }, options.pause);
     }
+
     function pauseSlider() {
         clearInterval(interval);
     }
-    
+
     $slideContainer
         .on('mouseenter', pauseSlider)
         .on('mouseleave', startSlider);
-    
+
     startSlider();
-    
-    
-    };
-    
+};
+
 }(jQuery));
